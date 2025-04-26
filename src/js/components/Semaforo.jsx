04fa603d9794c585e311cd15d980ac8564dd3bd3
aivandag <aivan.dev7@gmail.com>
2025-04-26 00:58:54 +0000

    semaforo 2.0

--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -2,13 +2,38 @@ import React, { useState } from "react";
 import "../../styles/semaforo.css";
 
 const Semaforo = () => {
-    const [color, setColor] = useState("red");
+    const [color, setColor] = useState("");
 
-return (
-    <div className="semaforo">
-<p style={{color:"white"}}>hola</p>
-    </div>
-);
+    return (
+
+        <div className="container">
+            <div className="barra"></div>
+            <div className="semaforo">
+                <div
+
+                    className={`luz roja ${color === "roja" ? "selected" : ""}`}
+                    onClick={() => setColor("roja")}
+
+                ></div>
+
+                <div
+
+                    className={`luz amarilla ${color === "amarilla" ? "selected" : ""}`} r
+                    onClick={() => setColor("amarilla")}
+
+                ></div>
+
+                <div
+
+                    className={`luz verde ${color === "verde" ? "selected" : ""}`}
+                    onClick={() => setColor("verde")}
+
+                ></div>
+
+            </div>
+        </div >
+
+    );
 };
 
 export default Semaforo;
